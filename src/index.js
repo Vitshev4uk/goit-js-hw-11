81c893be5b010431b4c46d16c3bc440b01d589fb
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-04-23 18:45:11 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -10,31 +10,44 @@ const btnLoadMore = document.querySelector('.load-more');
 const gallery = document.querySelector('.gallery');
 // const photoCard = document.querySelector('.photo-card');
 
-form.addEventListener('submit', (event) => {
-    const inputValue = input.value;
+let page = 1;
+const limit = 40;
+  
 
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const getImages = async () => {
-        try {
-            const response = await axios.get(`https://pixabay.com/api/?key=35632992-e10a39a36f128534b3670000b&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`);
-              if (response.data.hits.length === 0) {
-               Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-            }
-            console.log(response.data);
-            listImages(response.data.hits);
-            
-         }
-        catch (error) {
-            console.log(error);
-          
-        }
-    }
     getImages();
     form.reset();
 });
 
 
-btnLoadMore.disabled = true;
+const getImages = async () => {
+  try {
+    const inputValue = input.value;
+    const response = await axios.get(`https://pixabay.com/api/?key=35632992-e10a39a36f128534b3670000b&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${limit}&page=${page}`);
+    // if (response.data.hits.length = true) {
+    //   Notiflix.Notify.success('We have some images!!!');
+    // }
+       if (response.data.hits.length === 0) {
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    };
+    console.log(response.data);
+
+    const totalHits = response.data.totalHits;
+    const totalPages = totalHits / limit;
+    if (page > totalPages) {
+      Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+    }
+          
+    listImages(response.data.hits);
+  }
+  catch (error) {
+    console.log(error);
+          
+  }
+};
+
+// btnLoadMore.disabled = true;
 
     function listImages(image) {
         const markup = image.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
@@ -58,5 +71,18 @@ btnLoadMore.disabled = true;
         }).join('');
         return gallery.insertAdjacentHTML('beforeend', markup);
     }
+
+btnLoadMore.addEventListener('click', () => {
+  page += 1;
+  console.log(page);
+  getImages();
+  
+
+
+
+      
+ })
+
+
 
 
