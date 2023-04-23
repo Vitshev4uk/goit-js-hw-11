f33bdcc86f530dc33c900684585049e48e0765ac
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-04-23 22:43:05 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -12,47 +12,51 @@ const gallery = document.querySelector('.gallery');
 
 let page = 1;
 const limit = 40;
-  
+btnLoadMore.classList.add('hidden');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    getImages();
-    form.reset();
+input.addEventListener('click', () => {
+  form.reset();
+});
+btnSubmit.addEventListener('click', () => {
+  gallery.innerHTML = '';
 });
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  getImages();
+  Notiflix.Notify.success(`We found 500 images for you!!!`)
+});
 
 const getImages = async () => {
   try {
     const inputValue = input.value;
     const response = await axios.get(`https://pixabay.com/api/?key=35632992-e10a39a36f128534b3670000b&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${limit}&page=${page}`);
-    // if (response.data.hits.length = true) {
-    //   Notiflix.Notify.success('We have some images!!!');
-    // }
-       if (response.data.hits.length === 0) {
-      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    };
-    console.log(response.data);
 
     const totalHits = response.data.totalHits;
     const totalPages = totalHits / limit;
-    if (page > totalPages) {
-      Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+
+    if (response.data.hits.length === 0) {
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      btnLoadMore.classList.add('hidden');
+    } else if (response.data.hits.length !== 0) {
+      btnLoadMore.classList.remove('hidden');
     }
-          
+      else if (page > totalPages) {
+      Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+      btnLoadMore.classList.add('hidden');
+    };   
     listImages(response.data.hits);
   }
   catch (error) {
-    console.log(error);
-          
+    console.log(error);      
   }
 };
 
-// btnLoadMore.disabled = true;
 
     function listImages(image) {
         const markup = image.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
             return `<div class="photo-card">
-     <img src="${webformatURL}" alt="${tags}" loading="lazy" width="360px" />
+     <img src="${webformatURL}" href="${largeImageURL}" alt="${tags}" loading="lazy" width="360px" />
     <div class="info">
        <p class="info-item">
          <b>Likes: ${likes}</b>
@@ -74,14 +78,11 @@ const getImages = async () => {
 
 btnLoadMore.addEventListener('click', () => {
   page += 1;
-  console.log(page);
   getImages();
-  
+  console.log(page);
+});
 
 
-
-      
- })
 
 
 
